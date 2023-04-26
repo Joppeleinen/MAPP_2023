6aedce3ee3c73e004103f11d03009f67fb6cd246
sebjjan <sebbe.jansson1@gmail.com>
2023-04-26 20:18:16 +0200

    ändrade lmao

--- Archie/Assets/Scripts/MenuScripts/LoseMenu.cs
+++ Archie/Assets/Scripts/MenuScripts/LoseMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class LoseMenu : MonoBehaviour
 {
     public GameObject loseMenuUI;
-    public PlayerMovement lmao;
+    public PlayerMovement playerMov;
     public GameObject pauseButton;
     public Spawnpoint startingPoint;
 
@@ -21,7 +21,7 @@ public class LoseMenu : MonoBehaviour
     {
         Time.timeScale = 1f;
 
-        lmao.respawn();
+        playerMov.respawn();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         
 
@@ -32,7 +32,7 @@ public class LoseMenu : MonoBehaviour
         loseMenuUI.SetActive(false);
         pauseButton.SetActive(true);
         Time.timeScale = 1f;
-        lmao.respawnFromCheckpoint();
+        playerMov.respawnFromCheckpoint();
         SceneManager.LoadScene(5);
     }
     // Update is called once per frame
