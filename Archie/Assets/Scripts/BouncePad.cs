420c6654140807b5aff1f7f0b857a7e5b269d657
sebjjan <sebbe.jansson1@gmail.com>
2023-05-19 21:46:10 +0200

    level 3 mek

--- Archie/Assets/Scripts/BouncePad.cs
+++ Archie/Assets/Scripts/BouncePad.cs
@@ -28,7 +28,7 @@ public class BouncePad : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Box"))
         {
             
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
