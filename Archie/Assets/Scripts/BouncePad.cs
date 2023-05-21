17c98b72659b23f00c65f1751b1c12f6036d90c4
sebjjan <sebbe.jansson1@gmail.com>
2023-05-21 14:06:01 +0200

    Revert "level 3 mek"
    This reverts commit 420c6654140807b5aff1f7f0b857a7e5b269d657.

--- Archie/Assets/Scripts/BouncePad.cs
+++ Archie/Assets/Scripts/BouncePad.cs
@@ -28,7 +28,7 @@ public class BouncePad : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Box"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
